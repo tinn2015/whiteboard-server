69cec65cf96aa991f086330cfecb138e37535535
douqiting <douqiting@onlineinline.com>
2022-11-22 16:38:36 +0800

    dev数据库同步

--- src/common/config/env/local.ts
+++ src/common/config/env/local.ts
@@ -2,8 +2,8 @@ export default {
   DATABASE_USER: 'postgres',
   DATABASE_PASSWORD: 'pass123',
   DATABASE_NAME: 'qnwhiteboard',
-  DATABASE_PORT: 5433,
-  // DATABASE_HOST: 'localhost',
-  DATABASE_HOST: '172.25.9.11',
+  DATABASE_PORT: 5432,
+  DATABASE_HOST: 'localhost',
+  // DATABASE_HOST: '172.25.9.11',
   DATABASE_SYNC: true,
 };
